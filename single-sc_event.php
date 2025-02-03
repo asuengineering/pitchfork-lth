841aebbb4c058105d01aa8954ac7ee29ae3122c5
Steve Ryan <steve.ryan@asu.edu>
2025-02-03 11:26:37 -0700

    Post template for Sugar Calendar

---
+++ single-sc_event.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Learning Technology
+ * Sugar Calendar - Single
+ * Unstyled page template. Depends on blocks for page designs within custom post type.
+ * Based on page.php from the parent theme.
+ *
+ * @package pitchfork
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+get_header();
+?>
+
+	<main id="skip-to-content" <?php post_class(); ?>>
+
+		<?php
+
+		while ( have_posts() ) {
+
+			the_post();
+
+			// get_template_part( 'templates-global/global-banner' );
+
+			// Parse through the blocks on the page.
+			// If an acf/hero block is in the #1 position, do nothing.
+			// Otherwise display the post title.
+			$content_blocks = parse_blocks( $post->post_content );
+			$first_block_names = array('acf/hero', 'acf/hero-video', 'acf/hero-post', 'core/post-title');
+
+			if ( ! in_array( $content_blocks[0]['blockName'], $first_block_names )) {
+				the_title( '<div class="page-title"><h1 class="entry-title">', '</h1></div>' );
+			}
+
+			the_content();
+
+			// Display the edit post button to logged in users.
+			if ( is_user_logged_in() ) {
+				echo '<footer class="entry-footer"><div class="container"><div class="row"><div class="col-md-12">';
+				edit_post_link( __( 'Edit', 'pitchfork' ), '<span class="edit-link">', '</span>' );
+				echo '</div></div></div></footer><!-- end .entry-footer -->';
+			}
+		}
+
+		?>
+
+	</main><!-- #main -->
+
+<?php
+get_footer();
